7d3641160c6ae58cc92ab5aae012641458570e88
sekb555 <shrirang.ekbote04@gmail.com>
2025-02-05 12:39:40 +1300

    streamlined Logistic regression file

--- src/Train_LR.py
+++ src/Train_LR.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import joblib
 
-
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
@@ -12,10 +11,15 @@ import nltk
 from nltk.corpus import stopwords
 stop_words = stopwords.words('english')
 
-# Training the logistic regression model
+
+def load_data():
+    df = pd.read_csv("/Users/sekb/Desktop/processed_data.csv",
+                     encoding="ISO-8859-1")
+    df['Processed_Tweets'] = df['Processed_Tweets'].astype(str)
+    return df
 
 
-def logistic_regression(X, Y):
+def train_LR(X, Y):
     X_train, X_test, Y_train, Y_test = train_test_split(
         X, Y, test_size=0.1, random_state=42)
 
@@ -24,40 +28,45 @@ def logistic_regression(X, Y):
 
     pipeline.fit(X_train, Y_train)
 
+    y_pred = pipeline.predict(X_test)
+
+    return pipeline, X_train, X_test, Y_train, Y_test, y_pred
+
+
+def evaluate_model(con_mat):
+    false_neg = con_mat[1][0]
+    false_pos = con_mat[0][1]
+    true_neg = con_mat[0][0]
+    true_pos = con_mat[1][1]
+
+    precision = true_pos/(true_pos+false_pos)
+    recall = true_pos/(true_pos+false_neg)
+    F1 = 2*(precision*recall)/(precision+recall)
+
+    return precision, recall, F1
+
+
+def main():
+    df = load_data()
+    tweets = df['Processed_Tweets'].values
+    polarity = df['Polarity'].values
+
+    pipeline, X_train, X_test, Y_train, Y_test, y_pred = train_LR(
+        tweets, polarity)
+    con_mat = confusion_matrix(Y_test, y_pred)
+    precision, recall, F1 = evaluate_model(con_mat)
+
     training_accuracy = accuracy_score(Y_train, pipeline.predict(X_train))
     test_accuracy = accuracy_score(Y_test, pipeline.predict(X_test))
 
-    return confusion_matrix(Y_test, pipeline.predict(X_test)), training_accuracy, test_accuracy, pipeline
+    print("Precision:", round(precision*100, 2), "%")
+    print("Recall:", round(recall*100, 2), "%")
+    print("F1 Score:", round(F1*100, 2), "%")
+
+    print('Training accuracy: ', round(training_accuracy*100, 2), "%")
+    print('Test accuracy: ', round(test_accuracy*100, 2), "%")
+
+    joblib.dump(pipeline, open('data/logistic_regression_model.sav', 'wb'))
 
 
-# Load the processed data and encode the data
-df = pd.read_csv("data/processed_data.csv", encoding="ISO-8859-1")
-df['Processed_Tweets'] = df['Processed_Tweets'].astype(str)
-
-# Assign the text and the sentiment to varibles
-tweets = df['Processed_Tweets'].values
-polarity = df['Polarity'].values
-
-# Creating a confusion matrix for model evaluation
-con_mat, training_accuracy, test_accuracy, model = logistic_regression(
-    tweets, polarity)
-false_neg = con_mat[1][0]
-false_pos = con_mat[0][1]
-true_neg = con_mat[0][0]
-true_pos = con_mat[1][1]
-
-"""Evaluation Metrics"""
-precision = true_pos/(true_pos+false_pos)
-recall = true_pos/(true_pos+false_neg)
-F1 = 2*(precision*recall)/(precision+recall)
-print("Precision:", round(precision*100, 2), "%")
-print("Recall:", round(recall*100, 2), "%")
-print("F1 Score:", round(F1*100, 2), "%")
-
-print('Training accuracy: ', round(training_accuracy*100, 2), "%")
-print('Test accuracy: ', round(test_accuracy*100, 2), "%")
-
-
-"""Download the model"""
-filename = 'data/logistic_regression_model.sav'
-joblib.dump(model, open(filename, 'wb'))
+main()
